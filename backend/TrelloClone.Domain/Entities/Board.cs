e39987998b4c6c080e071f532c83f2a6ddfa58ac
magh <magh1521@gmail.com>
2026-03-09 19:50:39 -0700

    Add Lists collection to Board entity

--- backend/TrelloClone.Domain/Entities/Board.cs
+++ backend/TrelloClone.Domain/Entities/Board.cs
@@ -7,4 +7,5 @@ public class Board : BaseEntity
     public Guid OwnerId { get; set; }
     public User Owner { get; set; } = null!;
     public UserRole Role { get; set; } = UserRole.User;
+    public ICollection<List> Lists { get; set; } = new System.Collections.Generic.List<List>();
 }
